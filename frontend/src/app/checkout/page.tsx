9169f8d6d6d8e74fd5cb60aed078652ccf286df0
hameraidata <hameaidata@gmail.com>
2026-07-28 11:44:53 -0500

    fix: modificacion para culqi

--- frontend/src/app/checkout/page.tsx
+++ frontend/src/app/checkout/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { Navbar } from '@/components/Navbar';
 import { Footer } from '@/components/Footer';
 import { Checkout } from '@/components/Checkout';
